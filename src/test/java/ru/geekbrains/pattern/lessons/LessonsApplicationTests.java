9e858b88d101074ae0cf1650d4604c860f4aadd7
asbyvshev <barseto4ka@live.ru>
2020-05-06 17:18:31 +0300

    Initial commit

---
+++ src/test/java/ru/geekbrains/pattern/lessons/LessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.pattern.lessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
